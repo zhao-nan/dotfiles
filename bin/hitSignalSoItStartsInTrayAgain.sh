32253f122f82937569d80e9547881539de30ee44
Jonas Schiffl <jonas.schiffl@kit.edu>
2024-05-23 18:00:22 +0200

    hit Signal even harder

--- bin/hitSignalSoItStartsInTrayAgain.sh
+++ bin/hitSignalSoItStartsInTrayAgain.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/zsh
+killall signal-desktop
 sudo sed -i 's/signal-desktop --no-sandbox %U/signal-desktop --use-tray-icon --no-sandbox %U/g' /usr/share/applications/signal-desktop.desktop
